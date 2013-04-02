f21d2f6ac1ca1aaa9606432d6d43804f79dbdf64
Andrew_Shubin <shubandrey@gmail.com>
2013-04-02 15:24:25 +0400

    Added Control Work №1.
    Priority queue.

---
+++ Second semester/CW1/PriorityQueue/PriorityQueue.Tests/PriorityQueueTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PriorityQueue.Tests
+{
+    [TestClass]
+    public class PriorityQueueTest
+    {
+        Queue queue;
+        [TestInitialize]
+        public void Initialize()
+        {
+            queue = new Queue();
+        }
+        [TestMethod]
+        public void EnqueueTest()
+        {
+            Initialize();
+            queue.Enqueue(2, 1);
+            queue.Enqueue(1, 2);
+            Assert.AreEqual(queue.Head.Value, 2);
+
+        }
+        [TestMethod]
+        [ExpectedException(typeof(System.Exception))]
+        public void DequeueTest()
+        {
+            queue.Enqueue(2, 3);
+            queue.Enqueue(1, 4);
+            Assert.AreEqual(queue.Dequeue(), 1);
+        }
+    }
+}
---
+++ Second semester/CW1/PriorityQueue/PriorityQueue/PriorityQueue.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriorityQueue
+{
+    public class Queue
+    {
+        public PriorityQueueElement Head { get; private set; }
+
+        /// <summary>
+        /// Конструктор класса.
+        /// </summary>
+        public Queue()
+        {
+            this.Head = null;
+        }
+
+        public class PriorityQueueElement
+        {
+            public int Value { get; set; }
+            public PriorityQueueElement Next { get; set; }
+            public int Priority { get; set; }
+        }
+
+        /// <summary>
+        /// Добавление элемента в очереди.
+        /// </summary>
+        /// <param name="value">Значение элемента.</param>
+        /// <param name="priority">Приоритет элемента.</param>
+        public void Enqueue(int value, int priority)
+        {
+            PriorityQueueElement element = null;
+            element.Value = value;
+            element.Priority = priority;
+
+            if (Head == null)
+            {
+                element.Next = null;
+                Head = element;
+            }
+            else
+            {
+                if (element.Priority > Head.Priority)
+                {
+                    element.Next = Head;
+                    Head = element;
+                }
+                else
+                {
+                    PriorityQueueElement tmp = new PriorityQueueElement();
+                    tmp.Next = Head;
+
+                    while (tmp.Next.Priority < priority && tmp.Next != null)
+                        tmp = tmp.Next;
+
+                    element.Next = tmp.Next;
+                    tmp.Next = element;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Функция, удаляющая элемент очереди с наивысшим приоритетом.
+        /// </summary>
+        /// <returns>Значение удаленного элемента.</returns>
+        public int Dequeue()
+        {
+            if (Head == null)
+            {
+                throw new Exception("List is empty!");
+            }
+            else
+            {
+                int result = Head.Value;
+                Head = Head.Next;
+                return result;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
