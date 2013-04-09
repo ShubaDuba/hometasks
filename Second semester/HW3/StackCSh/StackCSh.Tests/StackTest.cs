15bd1a1ed7ff57548fc9ecc2528fc19c4c539233
Andrew_Shubin <shubandrey@gmail.com>
2013-04-09 20:46:03 +0400

    Added 3.1.

---
+++ Second semester/HW3/StackCSh/StackCSh.Tests/StackTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StackCSh.Tests
+{
+    [TestClass]
+    public class StackTest
+    {
+        private Stack stack;
+        [TestInitialize]
+        public void Initialize()
+        {
+            stack = new Stack();
+        }
+
+        /// <summary>
+        /// Тест добавления;
+        /// </summary>
+        [TestMethod]
+        public void PushTest()
+        {
+            stack.Push(3);
+            stack.Push(4);
+            Assert.AreEqual(stack.head.data, 4);
+            Assert.AreEqual(stack.head.next.data, 3);
+        }
+
+        /// <summary>
+        /// Тест получения значения из непустого стэка.
+        /// </summary>
+        [TestMethod]
+        public void PopTest()
+        {
+            stack.Push(3);
+            stack.Push(4);
+            Assert.AreEqual(stack.Pop(), 4);
+            Assert.AreEqual(stack.Pop(), 3);
+        }
+
+        /// <summary>
+        /// Тест получения значения из пустого стэка.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(System.Exception))]
+        public void PopFromEmptyStackTest()
+        {
+            stack.Push(2);
+            stack.Pop();
+            stack.Pop();
+        }
+    }
+}
---
+++ Second semester/HW3/StackCSh/StackCSh/Stack.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackCSh
+{
+    public class Stack
+    {
+        public StackElement head { get; set; }
+
+        /// <summary>
+        /// Конструктор класса.
+        /// </summary>
+        public Stack()
+        {
+            this.head = null;
+        }
+
+        public class StackElement
+        {
+            public int data { get; set; }
+            public StackElement next { get; set; }
+
+            /// <summary>
+            /// Конструктор класса.
+            /// </summary>
+            public StackElement(int value, StackElement nxt)
+            {
+                this.data = value;
+                this.next = nxt;
+            }
+        }
+
+        /// <summary>
+        /// Добавление элемента в стэк.
+        /// </summary>
+        /// <param name="n">Значение элемента.</param>
+        public void Push(int n)
+        {
+            StackElement element = new StackElement(n, head);
+            head = element;
+        }
+
+        /// <summary>
+        /// Взять значение из стэка.
+        /// </summary>
+        /// <returns>Значение "верхнего элемента".</returns>
+        public int Pop()
+        {
+            if (head == null)
+            {
+                throw new Exception("Stack is empty!");
+            }
+            else
+            {
+                int result = head.data;
+                head = head.next;
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// Обнуление стэка.
+        /// </summary>
+        public void Delete()
+        {
+            head = null;
+        }
+
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
