4fbb83cb30c4b10b58e50d2e8b3eb9d581840141
Andrew_Shubin <shubandrey@gmail.com>
2013-12-10 16:23:44 +0400

    Added Zero array elements counter.

---
+++ Third semester/HW_2/ArrayZeroElementsCount/ArrayZeroElementsCount.Tests/ArrayZeroElementsCountTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ArrayZeroElementsCount.Tests
+{
+    [TestClass]
+    public class ArrayZeroElementsCountTests
+    {
+        [TestMethod]
+        public void сountTest()
+        {
+            int[] array = new int[3];
+            array[0] = 1;
+            array[1] = 0;
+            array[2] = 0;
+            ZeroElementsCounter counter = new ZeroElementsCounter();
+            Assert.AreEqual(counter.count(array), 2);
+        }
+    }
+}
---
+++ Third semester/HW_2/ArrayZeroElementsCount/ArrayZeroElementsCount/ZeroElementsCounter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayZeroElementsCount
+{
+    public class ZeroElementsCounter
+    {
+        /// <summary>
+        /// Method which counts number of zero elements in array
+        /// </summary>
+        /// <param name="array">Considered array</param>
+        /// <returns>Number of zero elements</returns>
+        public int count(int[] array)
+        {
+            int tmpValue = 0;
+
+            for (int i = 0; i < array.Length; ++i)
+            {
+                if (array[i] == 0)
+                {
+                    ++tmpValue;
+                }
+            }
+
+            return tmpValue;
+        }
+    }
+}
