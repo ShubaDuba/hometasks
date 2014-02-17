ec677737118d425452d6d848732e0dba24dd0c2d
Andrew_Shubin <shubandrey@gmail.com>
2014-02-18 02:36:05 +0400

    First homework of 4th semester added.

---
+++ Fourth semester/HW_1/Factorial/Factorial/Factorial.fs
@@ -0,0 +1,10 @@
+﻿open System;
+
+let n = Int32.Parse(Console.ReadLine())
+
+let rec fact i =        
+  match i with        
+  | 1 -> 1        
+  | i -> i * fact(i-1)
+
+printfn "%i" (fact n)
---
+++ Fourth semester/HW_1/Fibonacci/Fibonacci/Fibonacci.fs
@@ -0,0 +1,10 @@
+﻿open System;
+
+let n = Int32.Parse(Console.ReadLine())
+
+let rec fib i =        
+  match i with        
+  | 1 | 2 -> 1        
+  | i -> fib(i-1) + fib(i-2)
+
+printfn "%i" (fib n)
