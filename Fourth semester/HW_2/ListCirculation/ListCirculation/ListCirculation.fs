70f570fece39584fc83a947f31af3107bdf47425
Andrew_Shubin <shubandrey@gmail.com>
2014-02-19 01:42:06 +0400

    Added list circulation and list of powers of two

---
+++ Fourth semester/HW_2/ListCirculation/ListCirculation/ListCirculation.fs
@@ -0,0 +1,11 @@
+﻿let input = [1; 3; 7; 13]
+
+let listCirc list = 
+    let rec circ list acc =
+        match list with
+        |(h::t) -> circ t (h::acc)
+        |[] -> acc
+    circ list []
+
+let output = listCirc input
+printfn "Circulated List :%A" output
---
+++ Fourth semester/HW_2/PowerOfTwo/PowerOfTwo/PowerOfTwo.fs
@@ -0,0 +1,10 @@
+﻿let input = 4.
+
+let listPowersOfTwo first =
+    let last = first + 4.
+    [for x in first..last -> 2. ** x]
+    
+
+let output = listPowersOfTwo input
+printfn "Powers Of Two List :%A" output
+
