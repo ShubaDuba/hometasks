cdb44a6006c39606aa7177da10ae6a6faf00b04d
Andrew_Shubin <shubandrey@gmail.com>
2014-09-13 18:51:36 +0400

    Added first task of the 3rd semester's first homework. Again ;(

---
+++ Third semester/second_chance/BinaryTreeWithIterator/BinaryTreeWithIterator.Tests/BinaryIteratorTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BinaryTreeWithIterator.Tests
+{
+    [TestClass]
+    public class BinaryIteratorTests
+    {
+        [TestMethod]
+        public void IteratorTest()
+        {
+            BinaryTreeNode tree = new BinaryTreeNode(
+                10,
+                new BinaryTreeNode(
+                    8,
+                    new BinaryTreeNode(
+                        6,
+                        new BinaryTreeNode(
+                            4,
+                            null,
+                            null),
+                        null),
+                    new BinaryTreeNode(
+                        23,
+                        null,
+                        null)),
+                new BinaryTreeNode(
+                    12,
+                    new BinaryTreeNode(
+                        7,
+                        null,
+                        null),
+                    new BinaryTreeNode(
+                        20,
+                        null,
+                        null)));
+            BinaryTreeBreadthFirstIterator Iterator = new BinaryTreeBreadthFirstIterator(tree);
+            int[] expected = new[] { 10, 8, 12, 6, 23, 7, 20, 4 };
+            int index = 0;
+            while (Iterator.HasNext())
+            {
+                Assert.AreEqual(expected[index++], Iterator.Current.GetValue());
+                Iterator.Next();
+            }
+
+            Assert.IsNull(Iterator.Current);
+        }
+    }
+}
